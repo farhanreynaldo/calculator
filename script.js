13b3323c34296217333cc0a5dbfea5115daea938
Farhan Reynaldo <farhanreynaldo@gmail.com>
2025-05-20 09:27:59 +0700

    Add display and calculation

--- script.js
+++ script.js
@@ -1,21 +1,65 @@
-const add = function (value_1, value_2) {
-    return value_1 + value_2;
-};
-
-const substract = function (value_1, value_2) {
-    return value_1 - value_2;
-};
-
-const multiply = function (value_1, value_2) {
-    return value_1 * value_2;
-};
-
-const divide = function (value_1, value_2) {
-    return value_1 / value_2;
-};
-
 const operate = function (value_1, operator, value_2) {
-    return operator(value_1, value_2);
+    if (operator === "+") {
+        return Number(value_1) + Number(value_2);
+    } else if (operator === "-") {
+        return Number(value_1) - Number(value_2);
+    } else if (operator === "*") {
+        return Number(value_1) * Number(value_2);
+    } else if (operator === "÷") {
+        return Number(value_1) / Number(value_2);
+    } else {
+        return undefined;
+    }
+};
+
+let display = "";
+let operator = "";
+let value_1 = "";
+let value_2 = "";
+
+const buttonMap = {
+    clear: "AC",
+    plusminus: "+/-",
+    percent: "%",
+    divide: "÷",
+    times: "×",
+    minus: "−",
+    plus: "+",
+    dot: ".",
+    equal: "=",
+    7: "7",
+    8: "8",
+    9: "9",
+    4: "4",
+    5: "5",
+    6: "6",
+    1: "1",
+    2: "2",
+    3: "3",
+    0: "0",
+};
+
+const populateDisplay = function (key) {
+    if (key === "AC") {
+        operator = "";
+        value_1 = "";
+        value_2 = "";
+    } else if (key === "=") {
+        let result = operate(value_1, value_2, operator);
+        value_1 = result;
+        value_2 = "";
+        operator = "";
+    } else if (["+", "-", "x", "÷"].includes(key)) {
+        operator = key;
+    } else {
+        if (!value_1) {
+            value_2 = key;
+        } else {
+            value_1 = key;
+        }
+    }
+    display = `${value_1}${operator}${value_2}`;
+    screenDiv.textContent = display;
 };
 
 const buttonList = [
@@ -23,15 +67,8 @@ const buttonList = [
     ["7", "8", "9", "times"],
     ["4", "5", "6", "minus"],
     ["1", "2", "3", "plus"],
-    ["zero", "dot", "equal"],
+    ["0", "dot", "equal"],
 ];
-
-// const buttonList = [
-//   ["clear", "7", "4", "1", "0"],
-//   ["plusminus", "8", "5", "2", "dot"],
-//   ["percent", "9", "6", "3", "equal"],
-//   ["divide", "times", "minus", "plus"],
-// ];
 
 buttonsContainer = document.querySelector(".buttons-container");
 for (const line of buttonList) {
@@ -39,10 +76,19 @@ for (const line of buttonList) {
     lineDiv.classList.add("line-container");
     buttonsContainer.appendChild(lineDiv);
     for (const button of line) {
-        const buttonDiv = document.createElement("div");
+        const buttonDiv = document.createElement("button");
         buttonDiv.classList.add(`button`);
-        buttonDiv.classList.add(`${button}`);
-        buttonDiv.textContent = button;
+        buttonDiv.id = button;
+        buttonDiv.textContent = buttonMap[button];
         lineDiv.appendChild(buttonDiv);
     }
 }
+
+screenDiv = document.querySelector(".screen");
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const buttonValue = buttonMap[button.id];
+        populateDisplay(buttonValue);
+    });
+});
